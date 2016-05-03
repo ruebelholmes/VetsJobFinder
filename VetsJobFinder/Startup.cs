a8570c09e427a34ece6d0b46450d999997c3e29a
Ruebel Holmes <treyholmes@yahoo.com>
2016-05-03 10:42:28 -0500

    Add project files.

---
+++ VetsJobFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VetsJobFinder.Startup))]
+namespace VetsJobFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
