774fae4e7c15fbd8657e8bf26eae79ac23d387a6
Ruebel Holmes <treyholmes@yahoo.com>
2016-05-03 11:29:19 -0500

    Added User and Employer models

---
+++ VetsJobFinder/Models/Employer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VetsJobFinder.Models
+{
+    public class Employer
+    {
+        public int EmployerId { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string EmployerEmail { get; set; }
+    }
+}
