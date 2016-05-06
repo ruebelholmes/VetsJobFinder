1ae3cb6a6d1dcec7c24c8b7bca23ccaccf9a5c64
Ruebel Holmes <treyholmes@yahoo.com>
2016-05-06 15:58:21 -0500

    Added Resume Model/VM

---
+++ VetsJobFinder/Models/Resume.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VetsJobFinder.Models
+{
+    public class Resume
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostedOn { get; set; } = DateTime.Now; 
+    }
+}
---
+++ VetsJobFinder/Models/ViewModels/ResumeCreateVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VetsJobFinder.Models.ViewModels
+{
+    public class CreateResumeVM
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+    }
+}
